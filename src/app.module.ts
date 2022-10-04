0803c522a766081385334c4483bd367494a3caa4
wahyudevlombok <wahyudipe6@gmail.com>
2022-10-04 11:19:56 +0800

    lagi ah pr

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { KomenModule } from './komen/komen.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, KomenModule],
   controllers: [AppController],
   providers: [AppService],
 })
