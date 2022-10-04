1c826efb0e90942902fc16a90b1513403127ac9e
DESKTOP-6R67050\Dev03 <ihfaz>
2022-10-04 12:48:04 +0700

    clean up branch

--- android/app/src/main/kotlin/com/example/sosprong/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.sosprong
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/core/utils/string_utils.dart
+++
@@ -1,8 +0,0 @@
-class Strings {
-  factory Strings() => _instance;
-  Strings.internal();
-  static final Strings _instance = Strings.internal();
-
-  static String exampletoas = "click me to show toash";
-  static String exampletoasmesage = "this is example toas gusy";
-}
