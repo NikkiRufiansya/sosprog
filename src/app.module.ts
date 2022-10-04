42ccef63b12364a5b1ccaad2efab8f76d87ed0a8
DESKTOP-6R67050\Dev03 <ihfaz>
2022-10-04 12:56:07 +0700

    copy from backend

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { StatusModule } from './status/status.module';
+
+@Module({
+  imports: [UsersModule, StatusModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
